de66e0e9c7f64964c2dfc74bb6b5a005db63a73d
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-08 10:39:50 +0900

    indexアクションにおいて、スクレイピングのメイン部分をメソッドにまとめた。さらにフォームからリンクを入力することで調べる商品ページを選択可能に

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,24 +4,9 @@ require 'open-uri'
 class ReviewsController < ApplicationController
     def index
       @reviews = Review.all
-      @contents = []
-      n = 1
-      user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.111 Safari/537.3"
-      while true do
-        url = "https://www.amazon.co.jp/%E3%80%90Amazon-co-jp%E9%99%90%E5%AE%9A%E3%80%91-%E3%82%B1%E3%83%AD%E3%83%83%E3%82%B0-%E7%B2%92%E6%84%9F%E3%81%97%E3%81%A3%E3%81%8B%E3%82%8A-%E3%82%AA%E3%83%BC%E3%83%88%E3%83%9F%E3%83%BC%E3%83%AB%E3%81%94%E3%81%AF%E3%82%93-900g/product-reviews/B0BVYG4YGW/ref=cm_cr_getr_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber=1"
-        formated_url = formatting_url(url, n)
-        begin
-          html = URI.open(formated_url, "User-Agent" => user_agent).read
-          doc = Nokogiri::HTML.parse(html)
-          reviews = doc.css('div.a-row.a-spacing-small.review-data')
-          break if reviews.empty?
-          @contents.concat(reviews)
-          n += 1
-        rescue OpenURI::HTTPError, Timeout::Error => e
-          break
-        end
+      if params[:url].present?
+        get_data
       end
-      @contents.map! { |html| strip_html_tags(html) }
     end
 
     def new
@@ -58,6 +43,27 @@ class ReviewsController < ApplicationController
 
     def formatting_url(url, n)
       url.sub(/pageNumber=\d+/, "pageNumber=#{n}")
+    end
+
+    def get_data
+      @contents = []
+      n = 1
+      user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.111 Safari/537.3"
+      while true do
+        url = params[:url]
+        formated_url = formatting_url(url, n)
+        begin
+          html = URI.open(formated_url, "User-Agent" => user_agent).read
+          doc = Nokogiri::HTML.parse(html)
+          reviews = doc.css('div.a-row.a-spacing-small.review-data')
+          break if reviews.empty?
+          @contents.concat(reviews)
+          n += 1
+        rescue OpenURI::HTTPError, Timeout::Error => e
+          break
+        end
+      end
+      @contents.map! { |html| strip_html_tags(html) }
     end
 
 end
