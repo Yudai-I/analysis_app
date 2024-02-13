22e53404ff04134e70a4efbb6d4e3be4f1508e0a
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-13 12:25:12 +0900

    ヘッダーのレイアウトとルーティング定義の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'homes/top'
   devise_for :users
-  root to: 'reviews#index'
+  root to: 'homes#top'
   resources :reviews, only: [:index, :create, :destroy]
   
 end
