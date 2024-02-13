b36da4b32dabefe7e45219901c99a7caf1b19697
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-13 12:31:14 +0900

    userコントローラ作成とルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :reviews, only: [:index, :create, :destroy]
-  
+  resources :users, only: [:index, :show]
 end
