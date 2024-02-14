75d29b21078abbf1ae9972e546e815b6c5ae6861
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-14 23:05:07 +0900

    レコメンデーションページを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+    current_user_age = current_user.age
+    current_user_gender = current_user.gender
+    @matched_reviews = Review.joins(:user).where(users: { age: current_user_age, gender: current_user_gender })
   end
 
   def show
