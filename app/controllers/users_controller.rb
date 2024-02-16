7597981db0682b08ee4abe35561b3f7e9f7fa317
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-17 01:02:51 +0900

    レビュー一覧から商品ページに飛ぶときに別タブを開くように修正 & カレントユーザーが保存しているレビューデータは、recommendationページにおいてアイコンによって区別できるようにロジックの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     current_user_age = current_user.age
     current_user_gender = current_user.gender
     @matched_reviews = Review.joins(:user).where(users: { age: current_user_age, gender: current_user_gender })
+    @index = "index"
   end
 
   def show
