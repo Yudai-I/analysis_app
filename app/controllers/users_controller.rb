eb91198935d0b8fc1b1ae09cd61ac7793dc590cc
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-17 01:51:27 +0900

    あるユーザーがほかのユーザーの詳細ページに遷移できないように制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:show]
   def index
     @active_nav = :recommendation
     current_user_age = current_user.age
@@ -11,4 +12,14 @@ class UsersController < ApplicationController
     @active_nav = :mypage
     @user = current_user
   end
+
+  private
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+
 end
