ea85f462406dccf54311be446eb831ad11408138
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-17 18:57:02 +0900

    ページネーション機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     current_user_age = current_user.age
     current_user_gender = current_user.gender
     @matched_reviews = Review.joins(:user).where(users: { age: current_user_age, gender: current_user_gender })
+    @pagenated_matched_reviews = @matched_reviews.page(params[:page])
     @index = "index"
   end
 
