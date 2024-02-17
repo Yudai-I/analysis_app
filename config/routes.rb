d57e2ae4b5093e7605551061a9cfe0067d0e63e3
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-17 12:13:37 +0900

    いいね機能用のルーティング設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :reviews, only: [:index, :create, :destroy]
+  resources :reviews, only: [:index, :create, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show]
 end
