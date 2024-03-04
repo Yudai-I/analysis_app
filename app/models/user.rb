c576b43709ef597c1e553dc3b718994305a27a98
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-03-05 01:52:12 +0900

    [修正大]サインアップ時の認証にemailの代わりにnameに変更 & 新規登録時のバリデーションメッセージを日本語に対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,33 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:authentication_keys => [:name]
   
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { in: 2..20 }
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   enum age: { "10代": 1, "20代": 2, "30代": 3, "40代": 4, "50代": 5, "60代": 6, "70代": 7, "80代": 8, "90代以上": 9 }
   enum gender: { 男: 1, 女: 2 }
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if username = conditions.delete(:name)
+      where(conditions).where(name: username).first
+    else
+      where(conditions).first
+    end
+  end
+
+
+  def email_required?
+    false
+  end
+  def email_changed?
+    false
+  end
+  def will_save_change_to_email?
+    false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_17_025130) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -38,7 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_17_025130) do
     t.integer "gender", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
